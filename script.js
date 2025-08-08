ed13cff6cf426e453948625b4c6dfe0844ae65ab
vivimolina22 <molinavivianalourdes@gmail.com>
2025-08-08 17:30:47 +0000

    I'm crying

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-console.log("Hello World");
+/*
+console.log("Hello World")
 
 //String has quotes/Number has no quotes
 let myTest = "123";
@@ -35,3 +36,69 @@ if(hour<12){
 }
 
 document.getElementById("greeting").innerText =greeting;
+*/
+
+const reviews = [
+    {
+        id: 1,
+        name: 'Susan Smith',
+        job:"water",
+        img: '#',
+        text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+    }
+    {
+        id:2,
+        name:'Anna Johnson',
+        job:'fire',
+        img:'#',
+        text:'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+    }
+    {
+        id:3,
+        name:'Peter Jones',
+        job:'air',
+        img:'#',
+        text:'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+    }
+]
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn') ;
+
+let currentItem = 0;
+
+//load initial item
+window.addEventListener('DOMContentLoaded', function () {
+    const item= reviews(currentItem);
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job
+    info.textContent = item.text;
+});
+
+function showPerson(person){
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+nextBtn.addEventListener('click', function(){
+    currentItems++;
+    if (currentItem > reviews.length-1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+pprevBtnaddEventListener('click', function() {
+    currentItem++;
+    if (currentItem<0 ){
+        currentItem =reviews.length - 1;
+        }
+});
